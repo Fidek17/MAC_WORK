97649853eb7e15df81352701884ed7034104dad3
Fabian137 <c.fabian.bel@gmail.com>
2023-09-03 16:51:59 -0600

    Programa 5 finalizado

--- ProgramasEstructuradeDatos/ArrayBidim/5programa.c
+++ ProgramasEstructuradeDatos/ArrayBidim/5programa.c
@@ -2,89 +2,125 @@
 #include <stdlib.h>
 #include <time.h>
 
-void escribirMatriz(float matriz[10][10][10], int n, int m, int p);
-void imprimirMatriz(float matriz[10][10][10], int n, int m, int p);
-float promedioMatriz(float matriz[10][10][10], int n, int m, int p);
-float incisoc(float matriz[10][10][10], int n, int m, int p);
+// n = filas; m = columnas; p = "ancho"
+void escribirMatriz(float matriz[5][5][5], int n, int m, int p);
+void imprimirMatriz(float matriz[5][5][5], int n, int m, int p);
+float promedioMatriz(float matriz[5][5][5], int n, int m, int p);
+float incisoc(float matriz[5][5][5], int m);
+float incisoD(float matriz[5][5][5]);
+void idEscuela(int escuela);
 
 int main(){
-    float matrix[10][10][10], sumasPricSec, promediog;
-    /*La suma en cada diagonal? 
-    O la suma de ambas diagonales*/
+    float matrix[5][5][5], promediosGlobEs, suma;
 
         escribirMatriz(matrix, 3, 4, 2);
-        printf("\n");
-        promediog = promedioMatriz(matrix,3,4,2);
-        promediog= incisoc(matrix,3,4,2);
+        promediosGlobEs = promedioMatriz(matrix,3,4,2); //promedio Global
+        printf("\n\nEl promedio global es de %.3f", promediosGlobEs);
 
+        promediosGlobEs = incisoc(matrix,4); //Promedio de las segundas calificaciones de la escuela elegida
+        printf("\tEl promedio de las segundas calificaciones de la escuela elegida es de %.3f \n\n", promediosGlobEs);
 
-    
+        suma = incisoD(matrix);
+        printf("\n\tSuma de calificaciones: %.3f\n", suma);
+
     return 0;
 }
 
-void escribirMatriz(float matriz[10][10][10], int n, int m, int p){
+void escribirMatriz(float matriz[5][5][5], int n, int m, int p){
     int i,j,k;
-    printf("\n");
     srand(time(NULL)); //Semilla para generar un numero random
     for(i=0; i<n; i++){
+        printf("\n");
         for(j=0; j<m; j++){
             for (k = 0; k < p; k++){
-                // printf(" a%d%d: ", i+1, j+1);
-                // scanf("%f", &matriz[i][j]);
-                matriz[i][j][k] = rand() % 24;
+                // printf(" Escuela %d \t alumno %d \t calificacion %dª: ", i+1, j+1, k+1);
+                // scanf("%f", &matriz[i][j][k]);
+                matriz[i][j][k] = rand() % 10; 
+                
+                /*Generamos los datos por comodidad pero hicimos las corridas con los datos de prueba, sólo hay que descomentar las dos líneas anteriores*/
             }
-            
         }
-        printf("\n");
     }
     imprimirMatriz(matriz, n, m, p);
 }
 
-void imprimirMatriz(float matriz[10][10][10], int n, int m, int p){
-    int i,j, k  ;
+void imprimirMatriz(float matriz[5][5][5], int n, int m, int p){
+    int i,j;
     for(i=0; i<n; i++){
-        printf("\n|");
+        printf("\n\n\n\t------------");idEscuela(i+1); printf("------------\n"); //imprime la escuela/universidad
+        printf("\n\t\t\tcalificaciones");
         for(j=0; j<m; j++){
-            for (k = 0; k < p; k++){
-                printf("\n");
-                printf("\t %.2f :: a%d%d%d", matriz[i][j][k], i,j,k);
-            }
-                printf("\t-:: Esc[%d]alumno[%d] --calif[%.2f :: %.2f]", i,j,matriz[i][j][0],matriz[i][j][1]);
-            printf("\n\n");
+            printf("\n\t alumno %d \t %.2f \t %.2f",j+1,matriz[i][j][0],matriz[i][j][1]);
         }
-        printf("\n\n|");
     }
 }
 
-float promedioMatriz(float matriz[10][10][10], int n, int m, int p){
+float promedioMatriz(float matriz[5][5][5], int n, int m, int p){
     int i, j, k;
-    float suma=0, promedio=0, promedioalum;
+    float suma=0;
 
     for(i=0; i<n; i++){
         for(j=0; j<m; j++){
             for(k=0; k<p; k++){
                 suma += matriz[i][j][k];
             }
-        printf("\n%f", suma);
-            
          }
     }
-
-    promedio = suma/(n*m*p);
-    return promedio;
+    // promedio = suma/(n*m*p);
+    return suma/(n*m*p);
 
 }
-float incisoc(float matriz[10][10][10], int n, int m, int p){
-int i, j, k, valor;
-float suma=0, promedio; 
+float incisoc(float matriz[5][5][5], int m){
+    int j, escuela;
+    float suma=0; 
 
-    printf("\n\nDame el valor de la escuela que deseas calcular: ");
-    scanf("%d", &valor);
+    printf("\n\n1-. UNAM");
+    printf("\n2-. IPN");
+    printf("\n3-. UAM\n");
+    printf("\nDame el número de escuela que deseas calcular: ");
+    scanf("%d", &escuela);
 
     for(j=0; j<m; j++){
-        suma+= matriz[valor][j][1];
+        suma+= matriz[escuela-1][j][1];
     }
-        promedio = suma/m;
-        printf("\n\nEl promedio de la escuela de la segunda calificacion de los alumnos de la escuela %d es: %f",valor, promedio);
-    return 0;
+    printf("\n\tEscuela: ");
+    idEscuela(escuela);
+    printf(" --\n");
+    return suma/m; //promedio
+}
+
+float incisoD(float matriz[5][5][5]){
+    int escuela, alumno, i,j;
+    float suma = 0;
+    printf("\nDame el número de escuela: ");
+    scanf("%d", &escuela);
+    printf("\nDame el número de alumno: ");
+    scanf("%d", &alumno);
+
+    printf("\n\tLa suma de las calificaciones del alumno %d, de la escuela ", alumno);
+    idEscuela(escuela);
+
+    for ( i = 0; i < escuela; i++){
+        for (j = 0; j < alumno; j++){
+            suma = matriz[i][j][0] + matriz[i][j][1];
+        }
+    }
+    return suma;
+}
+
+void idEscuela(int escuela){
+    switch (escuela){
+    case 1:
+        printf("UNAM");
+        break;
+    case 2:
+        printf("IPN");
+        break;
+    case 3:
+        printf("UAM");
+        break;
+    default:
+        printf("\nEscuela no existente");
+        break;
+    }
 }
