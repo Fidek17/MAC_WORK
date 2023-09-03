16c2fd881621e001169c6cca0e40ee0a9ab062fb
Fidek17 <fgilbertodv@gmail.com>
2023-09-02 23:11:39 -0600

    Ya quedo mas lindo

--- ProgramasEstructuradeDatos/ArrayBidim/ordenacionRenglones.c
+++ ProgramasEstructuradeDatos/ArrayBidim/ordenacionRenglones.c
@@ -1,32 +1,38 @@
 #include <stdio.h>
 
-void ingresardatos(int Arreglo[10][10], int B[30]);
-void imprimirdatos(int A[10]);
+void ingresardatos(int Arreglo[10][10], int B[30], int n);
+void imprimirdatos(int A[10][10], int n);
 
 int main(){
     int A[10][10],prueba[10];
-    int i, valor,j, pos=0;
+    int i, control,j, pos=0;
+    control=4;
 
-ingresardatos(A, prueba);
+    ingresardatos(A, prueba, control);
 
-printf("\n\nDame el renglon y la columna del valor que deseas hallar en el arreglo");
-scanf("%d", &i);
-scanf("%d", &j);  
+    printf("\n\nDame el valor del renglon del del dato que deseas hallar en el arreglo: ");
+    scanf("%d", &i);
+    printf("\n\nDame el valor de la columna del del dato que deseas hallar en el arreglo: ");
+    scanf("%d", &j);
+    
+
+    printf("\n\nLa matriz en cuestion es: \n\n");
+    imprimirdatos(A, control);
 
     pos = 1 +(((i-1)*(i))/2) + (j-1);
-printf("\n\nEl valor almacenado en %d %d es: %d en la posicion %d del arreglo unidimensional", i, j, prueba[pos], pos);
-    return 0; 
+    printf("\n\nEl valor almacenado en [%d] [%d] es: %d en la posicion %d del arreglo unidimensional", i, j, prueba[pos], pos);
+        return 0; 
 }
 
-void ingresardatos(int Arreglo[10][10], int B[30]){
+void ingresardatos(int Arreglo[10][10], int B[30], int n){
     int i, j, k = 1;
-    int n = 4;
+    
 
     for(i=1; i<n; i++){
         for(j=1; j<n; j++){
 
             if(j <= i ){
-                printf("\n\nDame el valor de la posicion %d  %d; ", i, j);
+                printf("\n\nDame el valor de la posicion [%d]  [%d]; ", i, j);
                scanf("%d", &Arreglo[i][j]);
 
                    
@@ -40,5 +46,19 @@ void ingresardatos(int Arreglo[10][10], int B[30]){
         }
 
     }
+
+}
+void imprimirdatos(int A[10][10], int n){
+    int i, j; 
+    for(i=1; i<n; i++){
+        for(j=1; j<n; j++){
+
+            printf("%d", A[i][j]);
+            printf("\t");
+        }
+        printf("\n");
+
+    }
+
 
 }
