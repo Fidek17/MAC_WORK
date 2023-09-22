481c3e82ddd49dbf9c0cbdfc5a3d7ebdf7890a51
Fidek17 <fgilbertodv@gmail.com>
2023-09-22 00:18:12 -0600

    Programa listas -> 3 algoritmos restantes(6,7,8)

--- ProgramasEstructuradeDatos/Listas/listasmenu.c
+++ ProgramasEstructuradeDatos/Listas/listasmenu.c
@@ -7,6 +7,11 @@ struct Nodo{
 };
 
 int menu();
+void insback(struct Nodo **P, struct Nodo **Q); 
+void mostrar(struct Nodo **P, struct Nodo **Q);
+void eliminarf(struct Nodo **P, struct Nodo **Q, struct Nodo **Aux);
+void insfront(struct Nodo **P, struct Nodo **Q, struct Nodo **Aux);
+void search(struct Nodo **P, struct Nodo **Q);
 
 int main(){
 int eleccion;
@@ -21,79 +26,28 @@ int i,n;
 
         switch(eleccion){
             case 1:
-            printf("\n\nLista Inicio\n\n"); 
-                printf("\nNumero de valores a ingresar: ");
-                scanf("%d", &n); 
-
-                for(i=1; i<=n; i++){
-                    Q=(struct Nodo *)malloc(sizeof(struct Nodo));
-                    printf("\n\nDame el valor a ingresar: "); 
-                    scanf("%d", &Q->info);
-                    Q->liga=P;
-                    P=Q;
-
-                }
+                printf("\n\nLista Inicio\n\n"); 
+                insback(&P,&Q);
 
             break;
             case 2:
-            printf("\n\nLa lista y sus elementos es la siguiente: \n");
-             i = 1;
-             Q=P;
-                while(Q!=NULL){
-                    printf("%d. %d", i, Q->info);
-                    Q=Q->liga;
-                    i++;
-                    printf("\n"); 
-                }
+                printf("\n\nLa lista y sus elementos es la siguiente: \n");
+                mostrar(&P,&Q);
             break;
             case 3:
-            printf("\n\n");
-            if(P==NULL){
-                printf("\nNo hay elementos");
-            }else{
-                if(P->liga==NULL){
-                    free(P);
-                    P=NULL;
-                }else{
-                    Q=P;
-                    Aux=NULL;
-                    while(Q->liga!=NULL){
-                        Aux=Q;
-                        Q=Q->liga; 
-                    }
-                    free(Q);
-                    Aux->liga=NULL;
-                }
-                printf("Se elimino el elemento correctamente");
-            }
+                printf("\n");
+                eliminarf(&P,&Q,&Aux);
             break;
             case 4:
-            printf("\n\n");
-            P=NULL;
-            printf("\nNumero de valores a ingresar: ");
-            scanf("%d", &n);
-                for(i=1; i<=n; i++){
-                    Aux =(struct Nodo *)malloc(sizeof(struct Nodo)); 
-                    printf("\n\nDame el valor que deseas ingresar: ");
-                    scanf("%d", &Aux->info); 
-                    Aux->liga=NULL;
-                    if(P==NULL){
-                        P=Aux;
-                    }else{
-                        Q=P;
-                        while(Q->liga!=NULL){
-                            Q=Q->liga;
-                        } 
-                    Q->liga=Aux; 
-                    }
-
-                }
+                printf("\n\nLista Final\n\n");
+                insfront(&P,&Q,&Aux);
             break;
             case 5:
-            printf("Eleccion 5");
+            printf("\n\nPueba  5\n\n");
+            search(&P,&Q);
             break;
             case 6:
-            printf("6 eleccion");
+            printf ("YA");
             break;
             case 7:
             printf("7 eleccion");
@@ -123,4 +77,98 @@ int res;
     printf("Ingresa la opcion que deseas ejecutar: "); 
     scanf("%d", &res);
     return res;
+}
+
+void insback(struct Nodo **P, struct Nodo **Q){
+    
+    int i,n;
+    printf("\nNumero de valores a ingresar: ");
+    scanf("%d", &n); 
+
+    for(i=1; i<=n; i++){
+        *Q=(struct Nodo *)malloc(sizeof(struct Nodo));
+        printf("\n\nDame el valor a ingresar: "); 
+        scanf("%d", &(*Q)->info);
+        (*Q)->liga=*P;
+        *P=*Q;
+
+    }
+
+}
+
+void mostrar(struct Nodo **P, struct Nodo **Q){
+                 int i = 1;
+             *Q=*P;
+                while((*Q)!=NULL){
+                    printf("%d. %d", i, (*Q)->info);
+                    *Q=(*Q)->liga;
+                    i++;
+                    printf("\n"); 
+                }
+}
+
+void eliminarf(struct Nodo **P, struct Nodo **Q, struct Nodo **Aux){
+            if(*P==NULL){
+                printf("\nNo hay elementos");
+            }else{
+                if((*P)->liga==NULL){
+                    free(*P);
+                    *P=NULL;
+                }else{
+                    *Q=*P;
+                    *Aux=NULL;
+                    while((*Q)->liga!=NULL){
+                        *Aux=*Q;
+                        *Q=(*Q)->liga; 
+                    }
+                    free(*Q);
+                    (*Aux)->liga=NULL;
+                }
+                printf("Se elimino el elemento correctamente");
+            }
+
+}
+
+void insfront(struct Nodo **P, struct Nodo **Q, struct Nodo **Aux){
+    int n,i; 
+                *P=NULL;
+            printf("\nNumero de valores a ingresar: ");
+            scanf("%d", &n);
+                for(i=1; i<=n; i++){
+                    *Aux =(struct Nodo *)malloc(sizeof(struct Nodo)); 
+                    printf("\n\nDame el valor que deseas ingresar: ");
+                    scanf("%d", &(*Aux)->info); 
+                    (*Aux)->liga=NULL;
+                    if(*P==NULL){
+                        *P=*Aux;
+                    }else{
+                        *Q=*P;
+                        while((*Q)->liga!=NULL){
+                            *Q=(*Q)->liga;
+                        } 
+                    (*Q)->liga=*Aux; 
+                    }
+
+                }
+}
+
+void search(struct Nodo **P, struct Nodo **Q){
+int dato;
+    if(*P==NULL){
+        printf("\nNo hay elementos\n");
+    }else{
+        printf("\n\nDame el elemento a buscar");
+        scanf("%d", &dato); 
+        *Q=*P;
+        while((*Q)->liga!=NULL && (*Q)->info!=dato){
+            *Q=(*Q)->liga; 
+        }
+        if((*Q)->info==dato){
+            printf("\nEL dato %d SI existe en la lista\n", dato); 
+        }else{
+            printf("\nEL dato %d NO existe en la lista\n", dato); 
+        }
+    }
+
+
 }
