6199d22e5d36f55800be007d78743eefa8f5d392
Fidek17 <fgilbertodv@gmail.com>
2023-11-13 15:27:25 -0600

    Revision genral, caso dots y caso puntos decimales seguidos

--- ProgramasEstructuradeDatos/Notacion/Prueba.c
+++ ProgramasEstructuradeDatos/Notacion/Prueba.c
@@ -6,9 +6,13 @@ struct Nodo{
     struct Nodo *der;
 };
 
-int revision(char *expresion);
+int revisionGeneral(char *expresion);
 int esOperador(char c);
 int dots(char *expresion);
+int esOperadorA(char c);
+int empezarOperador(char *expresion);
+int valoresValidos(char c);
+
 
 int main() {
     char expresion[100];
@@ -18,7 +22,9 @@ int main() {
 
     printf("La expresión ingresada es: %s", expresion); 
 
-    prueba = dots(expresion);
+    prueba = revisionGeneral(expresion);
+    //prueba = dots(expresion);
+    //prueba = empezarOperador(expresion);
     return 0;
 
 }
@@ -33,12 +39,25 @@ int revision(char *expresion){
     return 0; 
 }
 
+int esOperador(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')' || c == '^');
+}
+
+int esOperadorA(char c) {
+    return (c == '+' || c == '-' || c == '*' || c == '/'|| c == '^');
+}
+int valoresValidos(char c){
+    return(c == '+' || c == '-' || c == '*' || c == '/' || c == '^'|| c == '(' || c == ')' || c == '0'
+    || c == '1' || c == '2' || c == '3' || c == '4' || c == '5' || c == '6' || c == '7'
+    || c == '8' || c == '9'); 
+}
+
 int dots(char *expresion){
     int i = 0;
     int control; 
     control = 0;
 
-    while(expresion[i] != '\0' || control < 2){
+    while(expresion[i] != '\0' && control != 2){
        if(expresion[i] == '.'){
         control++; 
        }
@@ -50,13 +69,43 @@ int dots(char *expresion){
        printf("char en cuestion: %c", expresion[i]);
         i++;
     }
-if (control > 1){
-    printf("invalido ");
-}else{
-    printf("valido"); 
+    if (control == 2){
+        printf("invalido ");
+        return 1; 
+    }else{
+        printf("valido"); 
+        return 0;
+    }
+
 }
-    return 0; 
+
+
+
+int empezarOperador(char *expresion){
+    if(expresion[0] == '+' || expresion[0] == '^' || expresion[0] == '/'){
+        printf("Operacion invalida");
+        return 1; 
+    }else{
+        printf("Operacion valida");
+        return 0;
+    }
 }
-int esOperador(char c) {
-    return (c == '+' || c == '-' || c == '*' || c == '/' || c == '(' || c == ')');
+
+
+int revisionGeneral(char *expresion){
+    int i = 0;
+    int control; 
+    control = 1;
+
+    while(expresion[i] != '\0' && control == 1){
+        control = valoresValidos(expresion[i]);
+        i++;
+    }
+    if (control == 1){
+        printf("valido ");
+        return 0; 
+    }else{
+        printf("invalido"); 
+        return 1;
+    }
 }
